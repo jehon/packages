a9a87c4bbe8b0f3975e1fd393e78d6d731e81740
Jean Honlet <jehon@users.noreply.github.com>
2021-06-20 21:28:26 +0200

    refactor: install osmc

---
+++ docs/osmc.md
@@ -0,0 +1,16 @@
+# OSMC
+
+If using NFS, nfs://SERVER/path.
+For SMB, smb://user:password@SERVER/sharename/folder
+
+See https://discourse.osmc.tv/t/file-sharing-with-a-nas-media-server-windows-share-or-other-device/69732
+
+## Logs
+
+tail -f /home/osmc/.kodi/temp/kodi.log
+
+## TODO
+
+- Make deploy-all => deploy everything everywhere (by config) ==> jenkins job?
+- Install osmc from remote
+- Install ssh key from remote
--- setup-osmc.sh
+++ setup-osmc.sh
@@ -5,13 +5,20 @@ set -o errexit
 # shellcheck source=/dev/null
 . jh-lib
 
+# shellcheck source=/dev/null
 SSH_HOST="osmc"
+SSH_USER="osmc"
+SSH_PASS="osmc"
 
-header_start "Setup remote start..."
-./setup-remote.sh $SSH_HOST osmc osmc
+# header_start "Setup remote start..."
+# ./setup-remote.sh $SSH_HOST "$SSH_USER" "$SSH_PASS"
+# header_done
+
+header_start "Sending files to osmc..."
+sshpass -p "$SSH_PASS" scp "$JH_SECRETS_FOLDER"/crypted/osmc/osmc.env "$SSH_USER@$SSH_HOST":/home/osmc/osmc.env
+sshpass -p "$SSH_PASS" scp jehon-base-minimal/usr/bin/jh-osmc-system-setup "$SSH_USER@$SSH_HOST":/home/osmc/
 header_done
 
-header_start "Install jehon-system-tv..."
-scp "$JH_SECRETS_FOLDER"/osmc/jehon.env root@$SSH_HOST/etc/jehon/restricted/jehon.env
-ssh "root@$SSH_HOST" jh-osmc-system-setup
+header_start "Configure..."
+sshpass -p "$SSH_PASS" ssh "$SSH_USER@$SSH_HOST" ./jh-osmc-system-setup
 header_done
