b7553c0442c5f7b297606d3f8d3bfd1a5d96fc06
Jean Honlet <jehon@users.noreply.github.com>
2021-07-12 20:12:35 +0200

    fix: setup kiosk (wip)

--- setup-kiosk.sh
+++ setup-kiosk.sh
@@ -25,13 +25,70 @@ header_start "Setup remote start..."
 ./setup-remote.sh "$SSH_HOST" "$SSH_USER" "$SSH_PASS" "kiosk"
 header_done
 
-"$JH_PKG_FOLDER"/bin/jh-kiosk-configure-shares kiosk
+header_start "Preparing setup"
+header_start "Push files"
+scp "$JH_SECRETS_FOLDER"/crypted/kiosk/synology.credentials "root@$SSH_HOST":/etc/jehon/restricted/
+scp "$JH_SECRETS_FOLDER"/crypted/kiosk/kiosk.fstab "root@$SSH_HOST":/etc/jehon/
+scp "usr/share/jehon/rasperry/lightdm-autostart.conf" /etc/jehon
+header_done
 
-header_start "Run kickstart from kiosk github"
+header_start "Run scripts on setup"
+# Possible groups: nopasswdlogin
 ssh "root@$SSH_HOST" -T <<EOS
-	cd /opt/
-	wget https://raw.githubusercontent.com/jehon/kiosk/master/kickstart.sh -O /opt/kickstart.sh
-	chmod +x /opt/kickstart.sh
-	/opt/kickstart.sh && rm /opt/kickstart.sh
+	echo "Add mount's"
+	chmod 500 /etc/jehon/restricted/synology.credentials
+	jh-patch-file /etc/jehon/kiosk.fstab
+	mkdir -p /media/photo
+	mkdir -p /media/exploits
+	mount -a
+
+	echo "Add user kiosk"
+	useradd "kiosk" --create-home --groups "audio,video,plugdev,netdev,dip,cdrom"
+
+	echo "* Allow personnal key to log in as kiosk"
+	ln -s /etc/ssh/authorized_keys/jehon /etc/ssh/authorized_keys/kiosk
+
+	echo "* Patching sshd the old way"
+	jh-patch /usr/share/jehon/etc/ssh/sshd_config.d/jehon.conf
+
+# echo "* Disabling pi user"
+# passwd -l pi
 EOS
+header_done
+header_done
+
+header_start "Checkout"
+ssh "kiosk@$SSH_HOST" -T <<EOS
+	if [ -r kiosk ]; then
+		cd kiosk && git pull
+	else
+		git clone https://github.com/jehon/kiosk.git
+	fi
+EOS
+header_done
+
+header_start "Install as root"
+# rm -f /etc/systemd/system/default.target
+# ln -s /lib/systemd/system/graphical.target /etc/systemd/system/default.target
+ssh "root@$SSH_HOST" -T <<EOS
+	jh-patch /etc/jehon/lightdm-autostart.conf
+
+	kiosk/bin/kiosk-install-sudo.sh
+
+	echo "Redirect sound output to jack first card"
+	cp /usr/share/jehon/rasperry/asound.conf /etc/
+	chmod 640 /etc/asound.conf
+EOS
+header_done
+
+header_start "Install as user"
+# echo "arch=armv7l" >.npmrc
+
+ssh "kiosk@$SSH_HOST" -T <<EOS
+	kiosk/bin/kiosk-install.sh
+EOS
+header_done
+
+header_start "Starting the kiosk"
+ssh "kiosk@$SSH_HOST" "kiosk/kiosk-start.sh"
 header_done
